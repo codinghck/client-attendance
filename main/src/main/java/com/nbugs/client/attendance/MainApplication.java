5bd3b4455e986a19e66da67bce7858018b530dd6
hckisagoodboy <1459526461@qq.com>
2018-10-22 17:22:25 +0800

    add timer tasks

--- main/src/main/java/com/nbugs/client/attendance/MainApplication.java
+++ main/src/main/java/com/nbugs/client/attendance/MainApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author hongtiancai
  * @date 2018/10/22 2:26 PM
  */
+@EnableScheduling
 @SpringBootApplication
 public class MainApplication extends SpringBootServletInitializer {
   public static void main(String[] args) {
@@ -17,7 +19,6 @@ public class MainApplication extends SpringBootServletInitializer {
 
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    System.out.println("go===============================");
     return application.sources(MainApplication.class);
   }
 }
